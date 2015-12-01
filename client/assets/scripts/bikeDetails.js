8a17fbdaf4c474d829790bb11cf8636841a87f7b
fnasimi <farhad.nasimi@gmail.com>
2015-11-30 22:18:17 -0600

    modal dialog for contact owner

--- client/assets/scripts/bikeDetails.js
+++ client/assets/scripts/bikeDetails.js
@@ -3,7 +3,7 @@ $( document ).ready(function() {
  $('#testcomment').hide();
  
  $(function() {
-   $( "#start-date,#end-date" ).datepicker();
+ 	$("#start-date,#end-date").datepicker();
  });
  
  
@@ -13,14 +13,103 @@ $('#next').click(function(){
 	
 	$('#originalDetails').hide();		
 	$('#testdetails').show();
-		$('#originalComments').hide();
-			$('#testcomment').show();	
+	$('#originalComments').hide();
+	$('#testcomment').show();	
 });
 
 $('#previous').click(function(){		
 	$('#originalDetails').show();	
 	$('#testdetails').hide();
-		$('#originalComments').show();
-			$('#testcomment').hide();	
+	$('#originalComments').show();
+	$('#testcomment').hide();	
+});
 
+
+$(function() {
+  var dialog, form,
+    emailRegex = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/,
+    name = $( "#name" ),
+    email = $( "#email" ),
+    message = $( "#message" ),
+    allFields = $( [] ).add( name ).add( email ).add( message ),
+    tips = $( ".validateTips" );
+
+  function updateTips( t ) {
+    tips
+      .text( t )
+      .addClass( "ui-state-highlight" );
+    setTimeout(function() {
+      tips.removeClass( "ui-state-highlight", 1500 );
+    }, 500 );
+  }
+
+  function checkLength( o, n, min, max ) {
+    if ( o.val().length > max || o.val().length < min ) {
+      o.addClass( "ui-state-error" );
+      updateTips( "Length of " + n + " must be between " +
+        min + " and " + max + "." );
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  function checkRegexp( o, regexp, n ) {
+    if ( !( regexp.test( o.val() ) ) ) {
+      o.addClass( "ui-state-error" );
+      updateTips( n );
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  function addUser() {
+    var valid = true;
+    allFields.removeClass( "ui-state-error" );
+
+    valid = valid && checkLength( name, "username", 3, 16 );
+    valid = valid && checkLength( email, "email", 6, 80 );
+    valid = valid && checkLength( message, "message", 5, 16 );
+
+    valid = valid && checkRegexp( name, /^[a-z]([0-9a-z_\s])+$/i, "Username may consist of a-z, 0-9, underscores, spaces and must begin with a letter." );
+    valid = valid && checkRegexp( email, emailRegex, "eg. ui@jquery.com" );
+    valid = valid && checkRegexp( message, /^([0-9a-zA-Z])+$/, "Message field only allow : a-z 0-9" );
+
+    if ( valid ) {
+      $( "#users tbody" ).append( "<tr>" +
+        "<td>" + name.val() + "</td>" +
+        "<td>" + email.val() + "</td>" +
+        "<td>" + message.val() + "</td>" +
+      "</tr>" );
+      dialog.dialog( "close" );
+    }
+    return valid;
+  }
+
+  dialog = $( "#dialog-form" ).dialog({
+    autoOpen: false,
+    height: 450,
+    width: 400,
+    modal: true,
+    buttons: {
+      "Create an account": addUser,
+      Cancel: function() {
+        dialog.dialog( "close" );
+      }
+    },
+    close: function() {
+      form[ 0 ].reset();
+      allFields.removeClass( "ui-state-error" );
+    }
+  });
+
+  form = dialog.find( "form" ).on( "submit", function( event ) {
+    event.preventDefault();
+    addUser();
+  });
+
+  $( "#create-user" ).button().on( "click", function() {
+    dialog.dialog( "open" );
+  });
 });
